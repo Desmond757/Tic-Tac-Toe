b7b0cc84cddf29edc01da1f8e7f7ab6baba6dcf3
Victor Olabiyi <olabiyivictord@gmail.com>
2024-02-08 01:19:02 -0500

    Seperated code into functions

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -1,168 +1,17 @@
 #include <iostream>
 #include <pthread.h>
 using namespace std;
-
-int main(){
-    string Board[3][3] = {{" "," "," "},{" "," "," "},{" "," "," "}};
-    string P1, P2, P1name, P2name;
-    printf("WELCOME TO MY TIC-TAC-TOE GAME!!!\n"); 
-    printf("Supported characters are X and 0!\n"); 
-
-    // Player1 inputs
-    cout<< "Player 1, enter your name and character choice, seperated by space: ";
-    cin >> P1name >> P1;
-
-    // Conditional statements for inputs:::
-    if(P1 == "X" || P1 == "x"){
-        P2 = "O";
+const int board_size = 3;
+string  Board[board_size][board_size];
+void Build_Board(){
+    for(int row = 0; row < board_size; row++){
+        for (int col = 0; col < board_size; col++){
+            Board[row][col] = " ";
+        }
+        
     }
-    else if (P1 == "O" || P1 == "o"){
-        P2 = "X";
-    }
-    else{
-        cout << "Invalid character input for "<<P1name << endl;
-        return 0;
-    }
-
-    // Player2 inputs
-    cout<< "Player 2, enter your name:";
-    cin >> P2name;
-    
-    cout << P1name << ", you chose: " << P1 << endl;
-    cout << P2name << ", you are: " << P2 << endl;
-
-    // Create and Display Board
-    // for (int row = 0; row < 3; row++){
-    //     cout <<"+---+---+---+"<< endl;
-    //     cout<<"| ";//<<endl;
-    //     for (int col = 0; col < 3; col++){
-    //         //cout<<"|"<<endl;
-    //         cout<<Board[row][col]<< " | ";
-    //     }
-    //     cout<<endl;        
-    // }
-    // cout <<"+---+---+---+"<< endl;
-
-    /* Take players input*/
-
-    // Ask player to chose a position on the board (1-9)
-    // Use position chosen to place character on the board 
-    int P1position, P2position, position;
-    // cout<< P1name << ", enter your position (1-9): ";
-    // cin >> P1position;
-
-    // //Conditional Statement for invalid input for position
-    // if (P1position < 1 || P1position > 9){
-    //     cout << "Invalid position input for " << P1position << "try again (1-9): ";
-    //     cin >> P1position;
-    // }
-    // cout<< P2name << ", enter your position (1-9): ";
-    // cin >> P2position;
-
-    // //Conditional Statement for invalid input for position
-    // if (P2position < 1 || P2position > 9){
-    //     cout << "Invalid position input for " << P2position << "try again (1-9): ";
-    //     cin >> P2position;
-    // }
-    // // Check if Player 1 and Player2 play to the same position
-    // if(P1position == P2position){
-    //     cout << P1name << " and " << P2name << " cannot play into the same position!" << endl;
-    //     return 0;
-    // }
-    for(int row = 0; row < 3; row++){
-        for(int col = 0; col < 3; col++){
-
-        }
-        cout<< P1name << ", enter your position (1-9): ";
-        cin >> P1position;
-        //Conditional Statement for invalid input for position
-        if (P1position < 1 || P1position > 9){
-            cout << "Invalid position input for " << P1position << "try again (1-9): ";
-            cin >> P1position;
-        }
-        cout<< P2name << ", enter your position (1-9): ";
-        cin >> P2position;
-
-        //Conditional Statement for invalid input for position
-        if (P2position < 1 || P2position > 9){
-            cout << "Invalid position input for " << P2position << "try again (1-9): ";
-            cin >> P2position;
-        }
-        // Check if Player 1 and Player2 play to the same position
-        if(P1position == P2position){
-            cout << P1name << " and " << P2name << " cannot play into the same position!" << endl;
-            return 0;
-        }
-        //Conditional statement position for Player1
-        if(P1position == 1){
-            Board[0][0] = P1;
-        }
-        if(P1position== 2){
-            Board[0][1] = "X";
-        }
-        if(P1position == 3){
-            Board[0][2] = "X";
-        }
-        if (P1position == 4){
-            Board[1][0] = "X";
-        }
-        if (P1position == 5){
-            Board[1][1] = "X";
-        }
-        if (P1position == 6){
-            Board[1][2] = "X";
-        }
-        if (P1position == 7){
-            Board[2][0] = "X";
-        }
-        if (P1position == 8){
-            Board[2][1] = "X";
-        }
-        if (P1position == 9){
-            Board[2][2] = "X";
-        }
-
-        //Conditional statement position for Player1
-        if(P2position == 1){
-            Board[0][0] = P2;
-        }
-        if(P2position== 2){
-            Board[0][1] = P2;
-        }
-        if(P2position == 3){
-            Board[0][2] = P2;
-        }
-        if(P2position == 4){
-            Board[1][0] = P2;
-        }
-        if (P2position == 5){
-            Board[1][1] = P2;
-        }
-        if (P2position == 6){
-            Board[1][2] = P2;
-        }
-        if (P2position == 7){
-            Board[2][0] = P2;
-        }
-        if (P2position == 8){
-            Board[2][1] = P2;
-        }
-        if (P2position == 9){
-            Board[2][2] = P2;
-        }
-    }
-
-    // int row, col = 3;
-    // if (Board[row][col] == "X" || Board[row][col] == "O" || 
-    //     Board[row][col] == "x" || Board[row][col] == "o")
-    // {
-    //     cout << "Position already taken.";
-    // }
-    
-
-
-
-
+}
+void display_Board(){
     for (int row = 0; row < 3; row++){
         cout <<"+---+---+---+"<< endl;
         cout<<"| ";//<<endl;
@@ -172,9 +21,85 @@ int main(){
         }
         cout<<endl;        
     }
-    cout <<"+---+---+---+"<< endl;
+    cout <<"+---+---+---+"<< endl << endl;
+}
+void PlayersInput(string& P1, string& P1name, string& P2, string& P2name){
 
+    do{
+        // Player1 inputs
+        cout<< "Player 1, enter your name and character choice, seperated by space: ";
+        cin >> P1name >> P1;
+        // Convert the character choice to uppercase
+        for (char& c : P1) {
+            c = toupper(c);
+        }
+        /*Check for invalid character input*/
+        if(P1 == "X"){
+            P2 = "O";
+            break;
+        }
+        else if (P1 == "O"){
+            P2 = "X";
+            break;
+        }
+        else{
+            cout << "Invalid character input for player "<< P1name << ". Try again." <<endl;
+            //cin >> P1;
+        }
+    }while (true);
     
-   
-    return 0; 
+    // Player2 inputs
+    cout<< "Player 2, enter your name: ";
+    cin >> P2name;
+
+    /*Print out players characters*/
+    cout << P1name << ", you chose: " << P1 << endl;
+    cout << P2name << ", you are: " << P2 << endl;
+}
+
+void assign_Position(int P1position, int P2position, string& P1name, string& P2name){
+    do{
+        cout<< P1name << ", enter your position (1-9): ";
+        cin >> P1position;
+
+        //Conditional Statement for invalid input for position
+        if (P1position < 1 || P1position > 9){
+            cout << "Invalid position input for " << P1name << " try again (1-9). \n";
+            //cin >> P1position;
+            continue;
+        }
+
+        cout<< P2name << ", enter your position (1-9): ";
+        cin >> P2position;
+        //Conditional Statement for invalid input for position
+        if (P2position < 1 || P2position > 9){
+            cout << "Invalid position input for " << P2position << "try again (1-9): ";
+            //cin >> P2position;
+            continue;
+        }
+        while(P1position == P2position){
+            cout << P1name << " and " << P2name << " cannot play into the same position!" << endl;
+            if(P1position == P2position){
+            cout << P2name << " please enter a new position (1-9): ";
+            }
+            cin >> P2position;
+            //continue;
+        }
+        break;
+    }while(true);
+}
+int main(){
+    /* declarations:::::::::*/
+    int P1position, P2position;
+    string P1, P2, P1name, P2name;
+    printf("WELCOME TO MY TIC-TAC-TOE GAME!!!\n"); 
+    printf("Supported characters are X and 0!\n\n"); 
+    Build_Board();
+    display_Board();
+    PlayersInput(P1, P1name, P2, P2name);
+    assign_Position(P1position, P2position, P1name, P2name);
+    cout << "P1 possition= " <<P1position << endl;
+    cout << "P2 position = " <<P2position << endl;
+
+return 0;
 }
